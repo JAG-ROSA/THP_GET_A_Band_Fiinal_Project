f135e6eb1ab89992068c5123c3fc3c786d9ea14b
JAG-ROSA <76993925+JAG-ROSA@users.noreply.github.com>
2021-03-15 17:24:26 +0100

    Reservation table (#8)
    * generate Booking Model + migration
    
    * generate Availability Model + migration
    
    * generate seed
    
    * update migrations
    
    * update booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_142516) do
+ActiveRecord::Schema.define(version: 2021_03_15_144035) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,13 +21,36 @@ ActiveRecord::Schema.define(version: 2021_03_15_142516) do
     t.string "artist_name", null: false
     t.text "description"
     t.integer "hourly_price"
+    t.bigint "location_id"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_artists_on_email", unique: true
+    t.index ["location_id"], name: "index_artists_on_location_id"
     t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
+  end
+
+  create_table "availabilities", force: :cascade do |t|
+    t.datetime "date"
+    t.boolean "status"
+    t.bigint "artist_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_availabilities_on_artist_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_date"
+    t.integer "duration"
+    t.text "description"
+    t.bigint "user_id"
+    t.bigint "availability_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["availability_id"], name: "index_bookings_on_availability_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "locations", force: :cascade do |t|
