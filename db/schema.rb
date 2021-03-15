2902691f291bc3366f19d82c8988252bf1301e40
Caro407 <67237818+Caro407@users.noreply.github.com>
2021-03-15 15:11:19 +0100

    Devise artists (#4)
    * Install Devise for Artists
    
    * Create Artists Migration
    
    * Create Artists Controller
    
    * Create Devise views for Artists

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_131654) do
+ActiveRecord::Schema.define(version: 2021_03_15_133942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
+  create_table "artists", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "artist_name", null: false
+    t.text "description"
+    t.integer "hourly_price"
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_artists_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_artists_on_reset_password_token", unique: true
+  end
+
+   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "first_name"
