719d90b5adb27fbf1fd94a4d305a4488fbfcb1d8
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-16 09:59:31 +0100

    fonctional edit user views

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,26 @@ class UsersController < ApplicationController
   def create
   end
 
+  def edit
+    @user = helpers.current_user
+  end
+
+  def update_params
+    params.permit(:first_name, :last_name)
+  end
+
   def update
+    @user = helpers.current_user
+    @user.first_name = update_params[:first_name]
+    @user.last_name = update_params[:last_name]
+
+    if @user.save
+      redirect_to user_path(@user.id)
+      flash[:success] = "Your information has been udpated."
+    else
+      flash[:danger] = "Failure: " + @user.errors.full_messages.join(" ")
+      render :edit
+    end
   end
 
   def destroy
