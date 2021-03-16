c5210735892fca0314691afbe385a7364979b744
martinfzz <forget.l.martin@gmail.com>
2021-03-16 15:46:45 +0100

    Add devise artists controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   mount ForestLiana::Engine => '/forest'
   devise_for :users, path: 'users', controllers: { sessions: 'users/sessions', registrations:'users/registrations' }
-  devise_for :artists, path: 'artists'
+  devise_for :artists, path: 'artists', controllers: { sessions: 'artists/sessions', registrations:'artists/registrations' }
   resources :artists do
     resources :bookings
   end
