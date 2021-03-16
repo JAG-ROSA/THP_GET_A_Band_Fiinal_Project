d025903ac79cf579bc3276084b0d33b424c03619
Ariane <pikabooariane@hotmail.fr>
2021-03-16 18:37:50 +0200

    added edit_artist

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -10,8 +10,26 @@ class ArtistsController < ApplicationController
   end
 
   def update
+    @artist = current_artist
+    @artist.artist_name = params[:artist_name]
+    @artist.description = params[:description]
+    @artist.hourly_price = params[:hourly_price]
+    @artist.location = Location.find(params[:location])
+
+    if @artist.save
+      redirect_to artist_bookings_path(artist_id: @artist.id)
+      flash[:success] = "Your information has been udpated."
+    else
+      flash[:danger] = "Erreur : " + @artist.errors.full_messages.join(" ")
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  def edit
+    @artist = current_artist
+    @all_locations = Location.all
   end
 end
