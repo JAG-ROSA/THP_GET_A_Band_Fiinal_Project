de9609a4677b593b7ad39861dc7776aa2ff83ebb
martinfzz <forget.l.martin@gmail.com>
2021-03-16 15:42:05 +0100

    Change path after signin login

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   mount ForestLiana::Engine => '/forest'
-  devise_for :users, path: 'users'
+  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions', registrations:'users/registrations' }
   devise_for :artists, path: 'artists'
   resources :artists do
     resources :bookings
