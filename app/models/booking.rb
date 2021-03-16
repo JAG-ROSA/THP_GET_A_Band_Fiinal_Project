bbf96d7128fd7c865a6351aed729d7f577402d57
Ariane <pikabooariane@hotmail.fr>
2021-03-16 17:57:39 +0200

    added booking approval and instance method for price

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,8 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :availability
   has_one :artist, through: :availability, source: :artist
+
+  def total_price
+    return self.duration * self.artist.hourly_price
+  end
 end
