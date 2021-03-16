9e888adfa673ab2aaaf44554984db85210ea7525
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-16 16:57:20 +0100

    Add validator to check if start date is prior to end date

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -1,4 +1,11 @@
 class Availability < ApplicationRecord
   belongs_to :artist
   has_one :booking
+
+  validate :start_prior_to_end_date
+
+  def start_prior_to_end_date
+    errors.add(:start_date, "must be before end date") unless
+      start_date < end_date
+  end
 end
