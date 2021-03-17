67f7f93fd2e470591298a4cda068b15fb0d96a8d
JAG-ROSA <jag.f.rosa@gmail.com>
2021-03-17 11:13:18 +0100

    add stripe_customer_id column to Booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_174538) do
+ActiveRecord::Schema.define(version: 2021_03_17_100728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_174538) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.datetime "end_date"
+    t.string "stripe_customer_id"
     t.index ["availability_id"], name: "index_bookings_on_availability_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
