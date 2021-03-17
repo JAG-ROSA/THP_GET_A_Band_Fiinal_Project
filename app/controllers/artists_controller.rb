b7f89430b53a3fe68ce5d3032f5daa14f924af79
Caro407 <67237818+Caro407@users.noreply.github.com>
2021-03-17 09:20:05 +0100

    Create availability (#20)
    * Create Availabilities Controller
    
    * Create Availabities routes and view New
    
    * Add Availability end_date
    
    * Change Availability column name from :date to :start_date
    
    * Add validator to check if start date is prior to end date
    
    * Add validators on start and end dates which are mandatory
    
    * Update seed with new column and validators on Availabilities
    
    * methods to filter Available Artists on Availibity Model
    
    * Add an end_date column on Bookings

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
   def index
-    @artists = Artist.all
+    @artists = Artist.where(status: "approved")
+    #@artists = Availability.available_artists("2021-06-25 15:30:00".to_datetime, "2021-06-26 15:30:00".to_datetime)
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
-
-  mount ForestLiana::Engine => '/forest'
-  devise_for :users, path: 'users', controllers: { sessions: 'users/sessions', registrations:'users/registrations' }
-  devise_for :artists, path: 'artists', controllers: { sessions: 'artists/sessions', registrations:'artists/registrations' }
+  mount ForestLiana::Engine => "/forest"
+  devise_for :users, path: "users", controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+  devise_for :artists, path: "artists", controllers: { sessions: "artists/sessions", registrations: "artists/registrations" }
   resources :artists do
     resources :bookings
   end
   resources :users
+  resources :availabilities
 
-  root 'static_pages#index'
-
+  root "static_pages#index"
 end
