547fd7f9cca1573ae6226a43a3600fe24eb2f2bc
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-17 11:19:38 +0100

    Add form to specify dates on Artists Index

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,6 +2,7 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.where(status: "approved")
     #@artists = Availability.available_artists("2021-06-25 15:30:00".to_datetime, "2021-06-26 15:30:00".to_datetime)
+    @availability = Availability.first
   end
 
   def show
