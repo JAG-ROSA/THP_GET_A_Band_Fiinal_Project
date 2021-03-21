f51735a204d75056b9460c007c8b4df02e3e2c3a
JAG-ROSA <jag.f.rosa@gmail.com>
2021-03-21 18:25:43 +0100

    update create method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,7 +2,8 @@ class PicturesController < ApplicationController
   def create
     @artist = current_artist
     
-    @artist.update(params.require(:artist).permit(pictures:[] ) )   
+    # @artist.update(params.require(:artist).permit(pictures:[] ) )   
+    @artist.pictures.attach(params[:pictures] )   
     redirect_to artist_bookings_path(artist_id: @artist.id), success: "Picture well added!"
   end 
 end
