17af2a75199e4f3cf4613a5597298d638223b5a9
martinfzz <forget.l.martin@gmail.com>
2021-03-22 15:04:26 +0100

    Add sending message feature

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,7 +4,7 @@ class ConversationsController < ApplicationController
 
     @users = User.all.where.not(id: current_user)
     @conversations = Conversation.includes(:recipient, :messages)
-                                 .find(session[:conversations].first)
+                                 .find(session[:conversations])
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,13 @@ Rails.application.routes.draw do
     resources :availabilities
   end
   resources :users, only: [:create, :edit, :show, :update, :destroy]
-  resources :conversations, only: [:index, :create]do
+  resources :conversations, only: [:index, :create] do
     member do
       post :close
     end
+    resources :messages, only: [:create]
   end
+  
 
   root 'static_pages#index'
 
