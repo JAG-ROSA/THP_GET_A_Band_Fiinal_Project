c442ac7b2b0c2f08616f90633aaf2ddc1a072c96
JAG-ROSA <jag.f.rosa@gmail.com>
2021-03-22 10:53:15 +0100

    add destroy method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,9 +1,12 @@
 class PicturesController < ApplicationController
   def create
     @artist = current_artist
-    
-    # @artist.update(params.require(:artist).permit(pictures:[] ) )   
     @artist.pictures.attach(params[:pictures] )   
     redirect_to artist_bookings_path(artist_id: @artist.id), success: "Picture well added!"
   end 
+
+  def destroy
+    @picture = ActiveStorage::Attachment.find(params[:id])
+    @picture.purge
+  end
 end
