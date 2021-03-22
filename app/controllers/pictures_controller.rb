3fd7df87dde8d0fbaaedfe4fb157c77a995e193b
JAG-ROSA <jag.f.rosa@gmail.com>
2021-03-22 11:57:42 +0100

    add redirection

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,12 +1,22 @@
 class PicturesController < ApplicationController
+  before_action :set_artist, only: [:create, :destroy]
+
   def create
-    @artist = current_artist
     @artist.pictures.attach(params[:pictures] )   
     redirect_to artist_bookings_path(artist_id: @artist.id), success: "Picture well added!"
   end 
 
   def destroy
     @picture = ActiveStorage::Attachment.find(params[:id])
-    @picture.purge
+    if @picture.purge
+      redirect_to artist_bookings_path(artist_id: @artist.id), success: "Picture well deleted!"
+    else
+      flash[:danger] = "Something went wrong"
+    end
+  end
+
+  private
+  def set_artist
+    @artist = current_artist    
   end
 end
