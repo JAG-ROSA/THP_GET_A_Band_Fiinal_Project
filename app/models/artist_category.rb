e74be740c9012bcd6c2e0b73e13b0c7daf0918aa
Caro407 <caro.lefevre407@hotmail.fr>
2021-03-22 16:16:40 +0100

    Add validator to limit number of categories an Artist can select

--- app/models/artist_category.rb
+++ app/models/artist_category.rb
@@ -1,4 +1,12 @@
 class ArtistCategory < ApplicationRecord
   belongs_to :artist
   belongs_to :category
+
+  validate :set_categories_quota_per_artist
+
+  def set_categories_quota_per_artist
+    unless ArtistCategory.where(artist_id: artist.id).size < 3
+      errors.add(:category, "Vous ne pouvez pas sélectionner plus de 3 labels.")
+    end
+  end
 end
