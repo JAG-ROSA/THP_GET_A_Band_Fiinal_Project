b837c61db6eea7d47949d75295afaf2ee0e8c49c
martinfzz <forget.l.martin@gmail.com>
2021-03-26 15:29:34 +0100

    Remove approved_artist in user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,6 @@ class UserMailer < ApplicationMailer
     mail(to: @artist.email, subject: 'Bienvenue sur Get a Band !') 
   end
 
-  def approved_artist(artist)
-    @artist = artist
-    @url = 'https://get-a-band.herokuapp.com/'
-    mail(to: @artist.email, subject: 'Profil accepté sur Get a Band !') 
-  end
-
   def new_user(user)
     @user = user
     @url = 'https://get-a-band.herokuapp.com/'
